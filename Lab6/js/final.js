9fade2ac73a969436e137709cc6dd05f2f24679f
git <github.com>
2018-02-26 03:41:18 +0200

    Fixed a problem for lab 6.

--- Lab6/js/final.js
+++ Lab6/js/final.js
@@ -3,24 +3,27 @@ for (var i = 0; i < 9; i++) {
 	$("#content").append(div);
 }
 */
-var value = $("key").val();
+
 $("button").click(function() {
+	var value = $("#search").val();
 	render(value);
 });
 
-function render(para) {
-  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-  $.getJSON( flickerAPI, {
-    tags: para,
-    tagmode: "any",
-    format: "json"
-  })
-    .done(function( data ) {
-      $.each( data.items, function( i, item ) {
-        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#content" );
-        if ( i === 31 ) {
-          return false;
-        }
-      });
-    });
-}
+	function render(para) {
+	  	var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+	  	$.getJSON( flickerAPI, {
+	    	tags: para,
+	    	tagmode: "any",
+	    	format: "json"
+	 	})
+	    .done(function( data ) {
+	      $.each( data.items, function( i, item ) {
+	        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#content" );
+	        if ( i === 30) {
+	          return false;
+	        }
+	      });
+	    });
+	    $("#content").html("");
+
+	}
